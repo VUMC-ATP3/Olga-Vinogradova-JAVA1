2fbcf6ce88c6b0b505c0e98d24c4ebc8af50c99e
olga-vinogradova <104448936+olga-vinogradova@users.noreply.github.com>
2022-05-15 16:23:25 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,80 @@
+public class Main {
+    public static void main(String[] args) {
+        int vecumsSuns = 3;
+        String vardsSuns = "Reksis";
+//(mainiga tips) (mainiga nosaukums) = (vertiba);
+
+        double svarsSuns = 15.8;
+float augums = 60.2F;
+boolean irizsalcis=false;
+
+char dzimums = 'V';
+char dzimumsDivi = 'S';
+char vaditajaKategorija = 'B';
+
+
+
+        System.out.println("Mans suns");
+        System.out.println("Vecums");
+        System.out.println(vecumsSuns);
+        System.out.println("Vards");
+        System.out.println(vardsSuns);
+        System.out.println("Mana suna svars");
+        System.out.println(svarsSuns + " kg" + "\n");
+        System.out.println("mans suna augstums" + " " + augums + " cm");
+        System.out.println("Vai mans suns ir izsalcis");
+        System.out.println(irizsalcis);
+
+        int vecumsKakis = 5;
+        String vardsKakis = "Minka";
+        System.out.println("Mans kakis");
+        System.out.println("Vecums");
+        System.out.println(vecumsKakis);
+        System.out.println("Vards");
+        System.out.println(vardsKakis);
+
+
+        System.out.println("Kaka un suna kopejais vecums");
+        System.out.println(vecumsSuns+vecumsKakis);
+        System.out.println(vardsSuns+ " " +vardsKakis);
+
+
+
+
+
+        System.out.println("Hello Olga");
+byte monthInYear = 12;
+short autoPrice = 12312;
+long netIncome = 2147483699L;
+        System.out.println( monthInYear + "\n" + autoPrice + "\n" + netIncome);
+
+
+int atlikums;
+        int summa;
+        int reizinajums;
+        double dalijums;
+        int x = 5;
+        int y = 10;
+
+        double z = 5;
+        double q = 10;
+
+        double w = 12;
+
+        summa = x + y;
+        reizinajums = x * y;
+        dalijums = z / q;
+        atlikums = 12%5;
+
+        int skaitlis = 6;
+            System.out.println("Ja atbilde ir 0, sis ir para skaitlis " + 6%2);
+
+        System.out.println("SUMMA " + summa);
+            System.out.println("Reizinajums" + reizinajums);
+            System.out.println("Dalijums" + dalijums);
+            System.out.println("Atlikums" + atlikums);
+
+
+    }
+
+}
